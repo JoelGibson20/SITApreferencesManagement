926aeba44599ceb3de2523188fadf8fe20fb58e9
JoelGibson20 <joel.gibson20@gmail.com>
2022-03-27 14:29:47 +0100

    initial commit

---
+++ client/src/crypto-methods.js
@@ -0,0 +1,19 @@
+import {AES,SHA3} from "crypto-js";
+
+function submit
+
+/* const CryptoJS = require("crypto-js")
+/* define(function (require, exports, module) {
+    module.exports = {
+        encryptPreferences: function(pref, secretKey) {
+        return CryptoJS.AES.encrypt(pref,secretKey);
+        }
+    }
+});
+ */
+/* function encryptPreferences(pref, secretKey){
+    return CryptoJS.AES.encrypt(pref,secretKey);
+}
+
+/* export default encryptPreferences = encryptPreferences; */
+ 
